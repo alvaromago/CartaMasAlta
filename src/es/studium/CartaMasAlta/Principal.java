1ca23db59078ff6fcc8bbdd2e3a79190df0278dc
alvaromago <aalvaromago@gmail.com>
2023-04-19 08:34:07 +0200

    First commit

---
+++ src/es/studium/CartaMasAlta/Principal.java
@@ -0,0 +1,14 @@
+package es.studium.CartaMasAlta;
+
+public class Principal
+{
+
+	public static void main(String[] args)
+	{
+		// MVC
+		Modelo modelo = new Modelo();
+		MenuPrincipal menuPrincipal = new MenuPrincipal();
+		new Controlador(modelo, menuPrincipal);
+	}
+
+}
